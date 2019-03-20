ec7f50652e1be226b010b7aa4f70613034ef04d3
Z0neNp <ardorm79@hotmail.co.il>
2019-03-20 19:14:03 +0200

    Field agent is readygit add .git add .

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Pre-requisites for running Observer in development mode
+put Wireshark root dir to PATH.
 # Launch local instance in development mode
 Local instance uses SQLITE3 database.
 Command to launch rails server with connection to SQLITE3: `rails --environment=development`
--- lib/field_agent/main.py
+++ lib/field_agent/main.py
@@ -2,18 +2,62 @@ import pyshark
 import json
 import requests
 import ipaddress
-intelligence = {}
-cap = pyshark.FileCapture('dumpfile.pcap')
-sources = {'ip': int(ipaddress.IPv4Address('79.181.31.6')), 'incoming_req_count': 0}
-for p in cap:
-  sources['incoming_req_count'] += 1
-  print(f"{p['ip'].src}, {p['ip'].dst}")
-print(sources)
-try:
-  r = requests.post('http://localhost:3000/backend_api/dos_icmp_intelligence', json=sources)
-  print(r.status_code)
-  print(r.text)
-except requests.RequestException as e:
-  print(e)
-finally:
-  pass
+import os
+import time
+
+def captureFileUpdated(lastTime, filename):
+  updated = False
+  try:
+    latestTime = os.path.getmtime(filename)
+    if latestTime > lastTime:
+      updated = True
+  except TypeError:
+    updated = True
+  return updated, latestTime
+
+def getCaptureData(filename):
+  captureData = None
+  continueReporting = True
+  try:
+    captureData = pyshark.FileCapture(filename)
+  except FileNotFoundError as e:
+    print(f"Failed to acces a file with capture data. Reason, {e}.")
+    continueReporting = False
+  return captureData, continueReporting
+
+def getIntelligence(captureData):
+  intelligence = {'ip': int(ipaddress.IPv4Address(currentIp)), 'incoming_req_count': 0}
+  for p in captureData:
+    intelligence['incoming_req_count'] += 1
+  return intelligence
+
+def reportIntelligence(intelligence, observerUrl):
+  continueReporting = True
+  try:
+    res = requests.post(observerUrl, json=intelligence)
+    if res.status_code != 200:
+      continueReporting = False
+    message = json.loads(res.text)
+    continueReporting = message['continue_collection']
+  except requests.RequestException as e:
+    print(f"Failed to POST Observer. Reason, {e}.")
+    continueReporting = False
+  return continueReporting
+
+captureFilename = 'dumpfile.pcap'
+currentIp = '79.181.31.6'
+observerUrl = 'http://localhost:3000/backend_api/dos_icmp_intelligence'
+continueReporting = True
+lastModificationTime = None
+delayInSeconds = 10
+
+while continueReporting:
+  time.sleep(delayInSeconds)
+  captureUpdated, lastModificationTime = captureFileUpdated(lastModificationTime, captureFilename)
+  captureData = None
+  if captureUpdated:
+    captureData, continueReporting = getCaptureData(captureFilename)
+  if captureData is not None:
+    intelligence = getIntelligence(captureData)
+    print(intelligence)
+    continueReporting = reportIntelligence(intelligence, observerUrl)
